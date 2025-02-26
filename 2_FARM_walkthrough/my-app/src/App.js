8594d6204bcd5bef5b25b157d181f3690c1a1fda
myfishdrowned <saif.rahman.va@gmail.com>
2025-02-25 20:02:10 -0500

    added clock to app.js

--- 2_FARM_walkthrough/my-app/src/App.js
+++ 2_FARM_walkthrough/my-app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Welcome from './Welcome'; 
+import Clock from './Clock';
 
 function App() {
   return (
     <div> 
+      <h1>My React App</h1>
+      
       <Welcome name='Satoru' />
       <Welcome name='Yuji' />
       <Welcome name='Megumi' />
+      
+      <Clock />
     </div>
   );
 }
