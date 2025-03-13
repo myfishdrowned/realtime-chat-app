82d2f37f549437259a4be3b449f2124c7d7f6e49
myfishdrowned <saif.rahman.va@gmail.com>
2025-03-13 18:40:44 -0400

    Fixed authentication issues, updated server setup, fixed routes, and added middleware

--- chat-app/backend/src/index.js
+++ chat-app/backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-import {connectDB} from "./lib/db.js"
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
@@ -9,9 +9,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json()); 
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log("server is running on PORT: " + PORT)
+    console.log("server is running on PORT: " + PORT);
     connectDB();
 });
+
+console.log(app._router.stack.map(r => r.route?.path));
