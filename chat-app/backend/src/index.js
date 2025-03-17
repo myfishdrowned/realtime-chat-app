2acd5b63da96f5d69c5f988f21087fff098367c7
myfishdrowned <saif.rahman.va@gmail.com>
2025-03-16 22:40:17 -0400

    changed port number

--- chat-app/backend/src/index.js
+++ chat-app/backend/src/index.js
@@ -10,7 +10,7 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 app.use(express.json()); 
 app.use(cookieParser());
