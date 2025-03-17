2ab30f1a865f2b7421d619de42892565eb74c218
myfishdrowned <saif.rahman.va@gmail.com>
2025-03-16 23:16:24 -0400

    configured daisyui

--- chat-app/frontend/src/App.jsx
+++ chat-app/frontend/src/App.jsx
@@ -1,5 +1,19 @@
 const App = () => {
-  return <div className="text-red-500">Tailwind is working!</div>
+  return (
+    <div className="text-red-500">
+      Tailwind is working!
+      <div className="mt-4">
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
