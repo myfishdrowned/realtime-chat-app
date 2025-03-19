0f1f9273a96063f27be90ed7b98b98a1a0d4b2ca
myfishdrowned <saif.rahman.va@gmail.com>
2025-03-18 20:05:58 -0400

    created skeleton for pages and routes

--- chat-app/frontend/src/App.jsx
+++ chat-app/frontend/src/App.jsx
@@ -1,17 +1,19 @@
+import Navbar from "./components/Navbar";
+
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <div className="text-red-500">
-      Tailwind is working!
-      <div className="mt-4">
-        <button className="btn btn-neutral">Neutral</button>
-        <button className="btn btn-primary">Primary</button>
-        <button className="btn btn-secondary">Secondary</button>
-        <button className="btn btn-accent">Accent</button>
-        <button className="btn btn-info">Info</button>
-        <button className="btn btn-success">Success</button>
-        <button className="btn btn-warning">Warning</button>
-        <button className="btn btn-error">Error</button>
-      </div>
+    <div>
+      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
     </div>
   );
 };
