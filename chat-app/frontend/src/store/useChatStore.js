ef3e9df75eeda19fb89ff10b89f5722962ef366f
myfishdrowned <saif.rahman.va@gmail.com>
2025-03-29 16:33:02 -0400

    beginning send message and send image implementation

--- chat-app/frontend/src/store/useChatStore.js
+++ chat-app/frontend/src/store/useChatStore.js
@@ -34,6 +34,16 @@ export const useChatStore = create((set) => ({
     }
   },
   
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
 
