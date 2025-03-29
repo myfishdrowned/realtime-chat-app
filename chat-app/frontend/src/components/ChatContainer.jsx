03803438fffc62605e147f9e0c393b64cd43e0da
myfishdrowned <saif.rahman.va@gmail.com>
2025-03-29 16:20:08 -0400

    built preliminary chat container

--- chat-app/frontend/src/components/ChatContainer.jsx
+++ chat-app/frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore"; 
+import { useEffect } from "react";
+
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton"; 
+
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
-}
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+  } = useChatStore();
 
-export default ChatContainer
+
+
+  useEffect(() => {
+    if (!selectedUser?._id) return;
+
+    getMessages(selectedUser._id);
+
+    return () => {
+      
+    };
+  }, [selectedUser, getMessages]); 
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+
+  if (!selectedUser) {
+    return (
+      <div className="flex-1 flex items-center justify-center text-zinc-400">
+        Select a chat to start messaging
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
