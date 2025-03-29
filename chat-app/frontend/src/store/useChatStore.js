a52f13bf871215667a52f7c65a6e54c0cdfc5800
myfishdrowned <saif.rahman.va@gmail.com>
2025-03-29 17:13:02 -0400

    message send and image send fully functional, reflecting in mongodb

--- chat-app/frontend/src/store/useChatStore.js
+++ chat-app/frontend/src/store/useChatStore.js
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 import { useAuthStore } from "./useAuthStore";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -33,7 +33,6 @@ export const useChatStore = create((set) => ({
       set({ isMessagesLoading: false });
     }
   },
-  
   sendMessage: async (messageData) => {
     const { selectedUser, messages } = get();
     try {
@@ -44,6 +43,26 @@ export const useChatStore = create((set) => ({
     }
   },
 
+  subscribeToMessages: () => {
+    const { selectedUser } = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
+
+      set({
+        messages: [...get().messages, newMessage],
+      });
+    });
+  },
+
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage");
+  },
+
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
-
